56e918505833f720775f0af9a875bf1770450a82
COJUrama <uramacoju@gmail.com>
2024-01-22 16:04:16 +0100

    Testing out some JavaScript

---
+++ index.js
@@ -0,0 +1,23 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document
+  .querySelectorAll('img')[0]
+  .setAttribute(
+    'src',
+    `/16.1 The Dicee Challenge/Dice Images/dice${randomNumber1}.png`
+  );
+document
+  .querySelectorAll('img')[1]
+  .setAttribute(
+    'src',
+    `/16.1 The Dicee Challenge/Dice Images/dice${randomNumber2}.png`
+  );
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = '🔥 Player 1 Wins!';
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector('h1').innerHTML = ' Player 2 Wins! 🔥';
+} else {
+  document.querySelector('h1').innerHTML = 'Draw';
+}
